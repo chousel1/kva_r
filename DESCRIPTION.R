3472ffa7bc177bf4492c8df1a4e762fe0f5ad263
chousel1 <65519707+chousel1@users.noreply.github.com>
2020-11-09 06:13:16 -0800

    Delete DESCRIPTION.R

--- DESCRIPTION.R
+++
@@ -1,8 +0,0 @@
-Package: KVA_r
-Type: Package
-Title: Implements the Housel-Kanvesky Knowledge Value Added methodology in R
-Version: 0.1.0
-Author: Chris Housel
-Maintainer: Chris Housel <chrishouselwork@gmail.com>
-Description: This package is an implementation of the Housel-Kanevsky Knowledge Value Added (KVA) methodology in R
-License: GPL-2
