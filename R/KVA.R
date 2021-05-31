b5b4501466bfeaff2faf8d6f0894168495237257
chousel1 <65519707+chousel1@users.noreply.github.com>
2021-05-30 17:22:41 -0700

    Revised code for improved efficiency

--- R/KVA.R
+++ R/KVA.R
@@ -23,35 +23,33 @@ KVA <- function(no_emp, learn_time, pctauto, times_perf_year, avg_time_complete,
                    avg_time_complete = avg_time_complete, wage = wage,
                    revenue = revenue)
   df$tlearn_time <- df$no_emp * df$learn_time
-  total_knoweldge <- list()
+  total_knowledge <- list()
   for (i in 1:length(df$times_perf_year)) {
-    total_knoweldge[i] <- (df[i, 8] * df[i, 4]) * (1-df[i, 3])
+    total_knowledge[i] <- (df[i, 8] * df[i, 4]) * (1-df[i, 3])
   }
-  tst <- 1
-  for (i in 1:length(total_knoweldge)) {
-    tst <- rbind(tst, total_knoweldge[[i]])
+  tst <- total_knowledge[[1]]
+  for (i in 2:length(total_knowledge)) {
+    tst <- rbind(tst, total_knowledge[[i]])
   }
-  tst <- tst[2:length(tst)]
-  df$total_knoweldge <- tst
-  total_knoweldge <- list()
+  df$total_knowledge <- tst
+  expenses <- list()
   for (i in 1:length(df$no_emp)){
-    total_knoweldge[i] <- (df[i, 6] * df[i, 5] * df[i, 4]) * df[i, 1]
+    expenses[i] <- (df[i, 6] * df[i, 5] * df[i, 4]) * df[i, 1]
   }
-  tst <- 1
-  for (i in 1:length(total_knoweldge)) {
-    tst <- rbind(tst, total_knoweldge[[i]])
+  tst <- expenses[[1]]
+  for (i in 2:length(expenses)) {
+    tst <- rbind(tst, expenses[[i]])
   }
-  tst <- tst[2:length(tst)]
   df$expenses <- tst
-  rev_per_unit <- sum(revenue)/sum(df$total_knoweldge)
+  rev_per_unit <- sum(revenue)/sum(df$expenses)
   rok <- list()
   # Second argument to round specifies number of digits to round by
   for (i in 1:length(df$expenses)) {
     rok[i] <- round(((df[i, 9]/df[i, 10]) * 100), 2)
   }
   out <- list()
-  out[[1]] <- rev_per_unit
-  out[[2]] <- rok
-  out[[3]] <- df
+  out$rev_per_unit <- rev_per_unit
+  out$rok <- rok
+  out$dataframe <- df
   return(out)
 }
